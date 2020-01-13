9551f44a7b08daa96adc2954fcb8e0eb49caaa56
Alex Martinez <alexandr3martin3z@gmail.com>
2020-01-13 05:13:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,16 +20,16 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.find_by(attributes)  
+  movie = Movie.find_by(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = nil)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.string :title
   end
 end
 
